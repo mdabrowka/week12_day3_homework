5e65b896899e3bde38aa2ae00d793fbc76094f8d
Marta <martadabrowka@gmail.com>
2017-12-13 16:24:46 +0000

    displayed beer names on the page

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
-const app = function () {
-const url = "https://api.punkapi.com/v2/beers";
-makeRequest(url, requestComplete);
+let beers = [];
 
+const app = function () {
+  const url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
 
 }
 
@@ -16,6 +17,19 @@ const makeRequest = function(url, callback) {
 
 const requestComplete = function() {
   if (this.status !== 200) return;
+  const jsonString = this.responseText;
+  beers = JSON.parse(jsonString);
+    populateList(beers);
 }
+
+
+const populateList = function(beers) {
+   const ul = document.querySelector('#beer-list');
+   beers.forEach(function(beer) {
+     const li = document.createElement('li');
+     li.innerText = beer.name;
+    ul.appendChild(li);
+   });
+ }
 
 document.addEventListener('DOMContentLoaded', app);
